cb8dc0058a9fdf2ac30f8f1fd96c03532fab55f2
carrolp <carrolp@us.ibm.com>
2023-05-04 09:11:15 -0400

    Update src/request.js

--- src/request.js
+++ src/request.js
@@ -20,14 +20,9 @@
 //COMPARE const requestPromiseNative = require('request-promise-native');
 //COMPARE const requestTriggerFile = 'USE_LEGACY_REQUEST_LIBRARY';
 
-let axios;
-try {
-  axios = require('axios');
-}catch(err){
-  // sometimes it tries to load the esm version of axios, even thought we're doing a require(). i cant figure out why.
-  // ideally we'd migrate everything to esm so we dont have to deal with these cjs problems anymore.
-  axios = require('axios/dist/node/axios.cjs');
-}
+// In some scenarios Node tries to load the ESM version of axios when doing `require('axios')`, even though using `require` rather than `import`.
+// This require forces loading the CJS version as not everything is converted to ESM.
+const axios = require('axios/dist/node/axios.cjs');
 const Https = require('https');
 const Stream = require('stream');
 const merge = require('deepmerge');
